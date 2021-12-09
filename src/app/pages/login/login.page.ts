f94e901f3eb1e2fe414e3a23cee73d9876cbd77a
EfNik <s.nikolakeas@outlook.com>
2021-12-09 11:41:23 +0200

    Login and sign up connection with backend

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
+  }
+
+  Email: string;
+  Password: string;
+
+
+  sendData(){
+    // console.log(this.Email,this.Password)
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+
+    let body = {
+      email: this.Email,
+      password: this.Password
+    };
+
+    // this.http.post("http://192.168.1.2:8080/api/login",body).subscribe(data =>{
+    //   console.log(data);
+    // });
+    this.http.post("http://127.0.0.1:8080/api/login",body).subscribe(data =>{
+      console.log(data);
+    });
+    
   }
 
 }
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
   }
+
+  Email: string;
+  Password: string;
+  Firstname: string;
+  Lastname: string;
+
+
+  sendRegistration(){
+
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+
+    let body = {
+      email: this.Email,
+      password: this.Password,
+      fistname: this.Firstname,
+      lastname: this.Lastname
+    };
+
+    this.http.post("http://127.0.0.1:8080/api/signup",body).subscribe(data =>{
+      console.log(data);
+    });
+  }
+
+  // sendRegistration(){
+
+  //   let headers = new Headers();
+  //   headers.append('Content-Type','application/json');
+
+  //   let body = {
+  //     email: this.Email,
+  //     password: this.Password,
+  //     fistname: this.Firstname,
+  //     lastname: this.Lastname
+  //   };
+
+  //   this.http.post("http://192.168.1.2:8080/api/signup",body).subscribe(data =>{
+  //     console.log(data);
+  //   });
+  // }
 
 }
