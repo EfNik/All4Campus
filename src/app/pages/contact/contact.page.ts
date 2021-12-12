6a35f022118b69fdf04650ae5b78ffae68a40377
thanosgrm <94387795+thanosgrm@users.noreply.github.com>
2021-12-12 21:07:58 +0200

    Update contact.page.ts

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -8,17 +7,13 @@ import { Router } from '@angular/router';
 })
 export class ContactPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
-  ngOnInit() {
-    if(!!localStorage.getItem('token')){
-      return true
-    }
-    else{
-      
-      this.router.navigate(["../login"])
-      return false
-    }
+  ngOnInit() {}
+    sendData(){
+      console.log("sendreport")
+     }
   }
 
-}
+
+
